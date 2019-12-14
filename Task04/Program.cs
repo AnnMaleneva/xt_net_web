00964146d1991a25ae63a4505d2f07c6d0c7bb55
Аня Маленева <anewta-s@rambler.ru>
2019-12-14 19:15:06 +0400

    немного исправила

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -19,9 +19,10 @@ namespace Task04
             objectForSorting.OnStopSort += ObjectForSorting_OnStopSort;
 
 
-            //objectForSorting.SortArray(array, deleg);
-            Thread thread = new Thread(() => { objectForSorting.SortArray(array, deleg); });
-            thread.Start();
+            objectForSorting.SortArray(array, deleg);
+            //Thread thread = new Thread(() => { objectForSorting.SortArray(array, deleg); });
+            objectForSorting.SortInAdditionalThread(array, deleg);
+            //thread.Start();
          
             objectForSorting.OnStopSort -= ObjectForSorting_OnStopSort;     
 
@@ -49,82 +50,6 @@ namespace Task04
         private static void ObjectForSorting_OnStopSort()
         {
             Console.WriteLine("Sort is finish."); ;
-        }
-    }
-    class Sorting
-    {
-
-        public event Action OnStopSort = delegate { };
-
-        public void SortInAdditionalThread(int[] array, Func<int, int, bool> funcAction)
-        {
-            new Thread(() =>
-            {
-                SortArray(array, funcAction);
-                //OnStopSort?.Invoke();
-            }).Start();
-        }
-
-        public void SortArray(int[] array, Func<int, int, bool> funcAction)
-        {
-            OnStopSort?.Invoke();
-            if (funcAction == null)
-                throw new ArgumentException("funcAction", "Function argument cannot be null or empty.");
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = 0; j < array.Length - 1; j++)
-                {
-                    if (funcAction(array[j], array[j + 1]))
-                    {
-                        Swap(array, j, j + 1);
-                    }
-                }
-            }
-            
-        }
-
-        public void SortString(string[] array, Func<int, int, bool> funcString)
-        {
-            
-            if (funcString == null)
-                throw new ArgumentException("funcAction", "Function argument cannot be null or empty.");
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = 0; j < array.Length - 1; j++)
-                {
-
-
-                    if (funcString(array[j].Length, array[j + 1].Length))
-                    {
-
-                        if (array[j].Length != array[j + 1].Length)
-                        {
-                            string temp = array[j];
-                            array[j] = array[j + 1];
-                            array[j + 1] = temp;
-
-                            //IEnumerable<string> query = from word in array
-                            //                            orderby word.Length
-                            //                            select word;
-                        }
-
-
-                    }
-                }
-            }
-            
-        }
-
-        static void Swap(int[] array, int left, int right)
-        {
-            if (left != right)
-            {
-                int temp = array[left];
-                array[left] = array[right];
-                array[right] = temp;
-            }
         }
     }
 }
---
+++ Task04/Sorting.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    class Sorting
+    {
+        public event Action OnStopSort = delegate { };
+
+        public void SortInAdditionalThread(int[] array, Func<int, int, bool> funcAction)
+        {
+            new Thread(() => { SortArray(array, funcAction); });
+        }
+
+        public void SortArray(int[] array, Func<int, int, bool> funcAction)
+        {
+            OnStopSort?.Invoke();
+            if (funcAction == null)
+                throw new ArgumentException("funcAction", "Function argument cannot be null or empty.");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (funcAction(array[j], array[j + 1]))
+                    {
+                        Swap(array, j, j + 1);
+                    }
+                }
+            }
+
+        }
+
+        public void SortString(string[] array, Func<int, int, bool> funcString)
+        {
+
+            if (funcString == null)
+                throw new ArgumentException("Function argument cannot be null or empty.");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+
+
+                    if (funcString(array[j].Length, array[j + 1].Length))
+                    {
+
+                        if (array[j].Length != array[j + 1].Length)
+                        {
+                            string temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+
+                            //IEnumerable<string> query = from word in array
+                            //                            orderby word.Length
+                            //                            select word;
+                        }
+
+
+                    }
+                }
+            }
+
+        }
+
+        static void Swap(int[] array, int left, int right)
+        {
+            if (left != right)
+            {
+                int temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+            }
+        }
+    }
+}
